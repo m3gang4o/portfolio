d3a43d2f11c575f838918982c0e8712c39d58df4
Megan <gaom@unc.edu>
2025-07-30 09:38:35 -0400

    Fix missing CSS import in Feed.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header'; 
+import './Feed.css';
 import { setInternalNavigation, isInternalNavigation } from '../utils/cookies';
 
 const Feed = () => {
